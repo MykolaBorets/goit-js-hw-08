bdf0b7fcca58ef2fe50e6c6a0ec5c5f708e12f1b
MykolaBorets <mykola.borets@gmail.com>
2023-11-12 00:30:07 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,8 @@
 import vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-console.log('New play');
-
 const iframe = document.querySelector('iframe');
 const player = new vimeo(iframe);
-
-console.log('played the video!');
 
 const saveTimeThrottled = throttle(currentTime => {
   localStorage.setItem('videoTime', currentTime);
